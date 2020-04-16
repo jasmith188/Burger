dbc7815a7317730dfee4dd8e22fe2e08710a454b
Jimmy Smith <jasmith.188@gmail.com>
2020-04-16 19:04:06 -0400

    cp

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (name, devoured) VALUES ('Lamb Burger', false);
+INSERT INTO burgers (name, devoured) VALUES ('BBQ Bacon Burger', false);
+INSERT INTO burgers (name, devoured) VALUES ('Chorizo Burger', false);
